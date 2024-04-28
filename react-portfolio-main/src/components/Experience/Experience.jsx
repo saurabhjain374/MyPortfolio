583a95d829a44db52fa7d8dfe7684d942fbd374c
Saurabh Jain <saurabh.jain@onsolve.com>
2024-04-28 18:47:59 +0530

    fix bug

--- react-portfolio-main/src/components/Experience/Experience.jsx
+++ react-portfolio-main/src/components/Experience/Experience.jsx
@@ -33,11 +33,6 @@ export const Experience = () => {
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
-                  <ul>
-                    {historyItem.experiences.map((experience, id) => {
-                      return <li key={id}>{experience}</li>;
-                    })}
-                  </ul>
                 </div>
               </li>
             );
