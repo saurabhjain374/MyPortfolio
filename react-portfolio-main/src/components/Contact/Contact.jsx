69584bdfcd6184e1b2b2788d728d705e8f00ce9a
Saurabh Jain <saurabh.jain@onsolve.com>
2024-04-28 17:38:19 +0530

    update contact

--- react-portfolio-main/src/components/Contact/Contact.jsx
+++ react-portfolio-main/src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:saurabhjain374@gmail.com">myemail@email.com</a>
+          <a href="mailto:saurabhjain374@gmail.com">saurabhjain374@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/saurabhjain374/">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/saurabhjain374/">linkedin.com/saurabhjain374</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/saurabhjain374">github.com/myname</a>
+          <a href="https://github.com/saurabhjain374">github.com/saurabhjain374</a>
         </li>
       </ul>
     </footer>
